cfb1b10b979085e1f6cf17760b14b09f3482102f
Jonatan Delgado <delgadojonatan.b@gmail.com>
2025-05-21 17:32:24 +0200

    agregado funciones de clase game y player

--- sevenWonders.js
+++ sevenWonders.js
@@ -2,24 +2,6 @@ import { ageOneCards } from "./ageOneCards.js";
 
 const cardsDisplay = document.querySelector(".cardsDisplay");
 
-function displayCard(objCard) {
-  const div = document.createElement("div");
-  div.className = "card";
-  div.addEventListener("click", (e) => console.log(e.target));
-  const name = document.createElement("p");
-  name.textContent = objCard.name;
-  const type = document.createElement("p");
-  type.textContent = objCard.type;
-  const points = document.createElement("p");
-  points.textContent = objCard.points;
-
-  div.appendChild(name);
-  div.appendChild(type);
-  div.appendChild(points);
-
-  cardsDisplay.appendChild(div);
-}
-
 class Player {
   constructor(name) {
     this.name = name;
@@ -27,7 +9,15 @@ class Player {
     this.points = 0;
   }
 
-  selectCard() {}
+  selectCard(card) {
+    this.cards.push(card);
+    this.addCardPoints(card);
+  }
+
+  addCardPoints(points) {
+    this.points += points.points;
+    console.log(this);
+  }
 
   discardCard() {}
 }
@@ -43,21 +33,46 @@ class Cards {
 class Game {
   constructor(player1, player2) {
     this.players = [new Player(player1), new Player(player2)];
-    this.cards = this.showCards();
-    this.onTurn = 0;
+    this.actualTurn = 0;
+    this.cards = this.loadCards();
   }
 
-  changeTurn() {}
+  changeTurn() {
+    // realizo checkeo por si se agregase la posibilidad de tener mas jugadores
 
-  showCards() {
+    if (this.actualTurn === this.players.length - 1) this.actualTurn = 0;
+    else this.actualTurn++;
+  }
+
+  loadCards() {
     ageOneCards.forEach((card) => {
       const { nombre, type, puntos } = card;
       const newCard = new Cards(nombre, type, puntos);
-      displayCard(newCard);
+      this.displayCard(newCard);
     });
+  }
+
+  displayCard(objCard) {
+    const div = document.createElement("div");
+    div.className = "card";
+    div.addEventListener("click", () => {
+      this.players[this.actualTurn].selectCard(objCard);
+    });
+    const name = document.createElement("p");
+    name.textContent = objCard.name;
+    const type = document.createElement("p");
+    type.textContent = objCard.type;
+    const points = document.createElement("p");
+    points.textContent = objCard.points;
+
+    div.appendChild(name);
+    div.appendChild(type);
+    div.appendChild(points);
+
+    cardsDisplay.appendChild(div);
   }
 
   endGame() {}
 }
 
-new Game("jonatan", "Belen");
+const hola = new Game("jonatan", "Belen");
